c23444f742d352c52b5cfe671e80f24dfe37a552
Cayque Silva <cayque.ti@gmail.com>
2024-04-23 16:27:05 -0300

    finalização de componente sidebar

--- educa-feed-app/src/components/sidebar.jsx
+++ educa-feed-app/src/components/sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './sidebar.module.css'
 
 export function Sidebar(){
@@ -9,12 +11,14 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/cayquesilva.png"/>
                 <strong>Cayque Silva</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
